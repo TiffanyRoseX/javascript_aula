f3d3035c8d246dac40af49374e458be12f7f5c33
Tiff <86427660+TiffanyRoseX@users.noreply.github.com>
2021-09-22 16:04:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,163 @@
+
+
+
+ //FUNCTION ONMOUSEOVER E ONMOUSEOUT
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigada por passar o mouse!";
+   elemento.innerHTML = "Obrigada por passar o mouse!";
+    //alert("trocar texto");
+}
+
+function voltar(elemento){
+   elemento.innerHTML = "Passe o mouse aqui!";
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui!";
+}
+
+
+//function onload//
+/*
+function load(){
+    alert("pagina carregada");
+}
+
+
+//FUNCTION ONCHANGE//
+/*
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
+
+
+
+
+//FUNCTION COM ONCLICK//
+
+/*
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigada por clicar";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigada por clicar!");
+}
+
+function redirecionar(){
+   // window.open("https://twitter.com/lovztwice");
+   // window.location.href = "https://twitter.com/lovztwice/";
+}
+/*
+
+
+
+//FUNCTION//
+/*
+function soma(n1,n2){
+    return n1 + n2
+}
+
+function  validaIdade(idade){
+    var validar;
+    if (idade >=18){
+    validar = true
+}else {
+    validar = false
+}
+return validar;
+}
+
+var idade = prompt("Qual é a sua idade?");
+console.log(validaIdade(idade));
+/*
+
+
+//Date//
+/*
+var d = new Date();
+alert(d.getMonth()+1);
+(d.getDay()+);
+alert(d.getHours());
+alert(d.getMinutes());
+/*
+
+/*
+
+//repetição//
+var count;
+for(count= 1; count <= 5; count++) {
+    alert(count);
+
+}; /*
+
+
+//condicionais//
+/*
+var count = 0;
+while (count <=5) {
+    console.log(count);
+    count = count + 1;
+} /*
+
+
+
+
+//condicionais//
+/*
+var idade = prompt("Qual sua idade?");
+var idade = 18;
+if (idade >= 18) {
+    alert("maior de idade");
+}else {
+    alert("menor de idade");
+}; /*
+
+
+//arrays//
+/*
+var frutas = [{ nome:"maça",  cor:"vermelha"}, { nome:"uva",  cor:"roxa"}]
+console.log(frutas.nome);
+alert(frutas[0].nome); /*
+
+
+
+//arrays//
+/*
+var fruta = { nome:"maça",  cor:"vermelha"}
+console.log(fruta.nome);
+console.log(fruta.cor);
+alert(fruta.nome);
+alert(fruta.cor); /*
+
+
+
+//arrays//
+//var lista = ["maça" , "pera" , "laranja"];
+//lista.push("uva");
+
+//lista.pop();
+
+//console.log(lista);
+
+//console.log(lista.toString());
+
+//console.log(lista.join(" "));
+
+//console.log(lista[1]);
+
+//alert(lista[1]);
+
+
+//introdução//
+//var nome = "Tiffany Rose";
+//var idade = 20;
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo"
+//var n1 = 3;
+//var n2 = 6;
+
+//introdução//
+/*
+alert(n1 * n2);
+alert( nome +  " tem " + idade + " anos " );
+alert( idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
+alert(frase.replace("Japão","Brasil"));  */
